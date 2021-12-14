27261d34b5df4e0c747f9e0ae91f25c924e83496
Sid Saythongphet <sid.saythongphet@gmail.com>
2021-12-13 21:49:52 -0500

    commit tree e6311f656769edc252c2dd36fd3f9c5e24ce9685

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+    if(streetNumber < 42){
+        return 42 - streetNumber
+    } else {
+        return streetNumber - 42
+    }
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return distanceFromHqInBlocks(streetNumber) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if(destination > start) {
+        return (destination - start) * 264
+    } else {
+        return (start - destination) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if(distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    } else if(distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    } else if(distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
